3e11aad6b00a30e51a04a5270d3aa8b7dcb0d8b7
Ezgi Buse Parlakçı <33104218+EzgiBuse@users.noreply.github.com>
2023-10-24 22:43:15 +0300

    Add files via upload

---
+++ Controllers/CategoryController.cs
@@ -0,0 +1,99 @@
+﻿using BulkyWeb.Data;
+using BulkyWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDBContext _db;
+        public CategoryController(ApplicationDBContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            List<Category> objCategoryList = _db.Categories.ToList();
+            return View(objCategoryList);
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null && id == 0)
+            {
+                return NotFound();
+            }
+            Category categoryfromdb = _db.Categories.Find(id);
+            if (categoryfromdb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryfromdb);
+        }
+
+        [HttpPost,ActionName("Delete")]
+        public IActionResult DeletePOST(int? id)
+        {
+            Category obj = _db.Categories.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            _db.Categories.Remove(obj);
+           
+                _db.SaveChanges();
+            TempData["Success"] = "Category Deleted Successfully";
+            return RedirectToAction("Index");
+            
+           
+
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if(id==null && id == 0)
+            {
+                return NotFound();
+            }
+            Category categoryfromdb = _db.Categories.Find(id);
+            if(categoryfromdb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryfromdb);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Category obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(obj);
+                _db.SaveChanges();
+                TempData["Success"] = "Category Updated Successfully";
+                return RedirectToAction("Index");
+            }
+            return View();
+
+        }
+
+
+        public IActionResult Create() {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Category obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                TempData["Success"] = "Category Created Successfully";
+                return RedirectToAction("Index");
+            }
+                return View() ; 
+            
+        }
+    }
+}
